705c4955ab2840bf333060bae6d32efef8232ebc
Eugene <jekahy@gmail.com>
2017-07-19 12:48:24 +0200

    add validate and generate buttons to CreditCardView

--- CardValidator/CreditCardView.swift
+++ CardValidator/CreditCardView.swift
@@ -12,17 +12,61 @@ import PureLayout
 class CreditCardView: UIView {
     
     let inputField = CreditCardInputField()
+    let validateButton = UIButton(type: .custom)
+    let generateButton = UIButton(type: .custom)
+    let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+    
     let edgeMargin:CGFloat = 10
-
+    let buttonHeight:CGFloat = 30
+    
     init()
     {
         super.init(frame: CGRect.zero)
+        
         addSubview(inputField)
+        addSubview(validateButton)
+        addSubview(generateButton)
+        addSubview(activityIndicator)
+        
+        activityIndicator.isHidden = false
+        
+        backgroundColor = UIColor.purple
+        
+        validateButton.backgroundColor = UIColor.cyan
+        validateButton.setTitle("Validate", for: .normal)
+        
+        generateButton.backgroundColor = UIColor.cyan
+        generateButton.setTitle("Generate", for: .normal)
+        
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints()
+    {
         inputField.autoPinEdge(toSuperviewEdge: .top, withInset:edgeMargin)
         inputField.autoPinEdge(toSuperviewEdge: .leading, withInset:edgeMargin)
         inputField.autoPinEdge(toSuperviewEdge: .trailing, withInset:edgeMargin)
         inputField.autoMatch(.height, to: .height, of: self, withMultiplier: 0.5)
-        backgroundColor = UIColor.purple
+        
+        validateButton.autoPinEdge(.top, to: .bottom, of: inputField, withOffset:edgeMargin)
+        validateButton.autoSetDimension(.width, toSize: 60, relation: .greaterThanOrEqual)
+        validateButton.autoSetDimension(.height, toSize: buttonHeight)
+        validateButton.autoPinEdge(toSuperviewEdge: .trailing, withInset:edgeMargin)
+        
+        
+        generateButton.autoPinEdge(.top, to: .bottom, of: inputField, withOffset:edgeMargin)
+        generateButton.autoSetDimension(.width, toSize: 60, relation: .greaterThanOrEqual)
+        generateButton.autoSetDimension(.height, toSize: buttonHeight)
+        generateButton.autoPinEdge(toSuperviewEdge: .leading, withInset:edgeMargin)
+        
+        generateButton.autoMatch(.width, to: .width, of: validateButton)
+        
+        activityIndicator.autoAlignAxis(.horizontal, toSameAxisOf: generateButton)
+        activityIndicator.autoAlignAxis(.vertical, toSameAxisOf: self)
+        activityIndicator.autoSetDimensions(to: CGSize(width: buttonHeight, height: buttonHeight))
+        activityIndicator.autoPinEdge(.left, to: .right, of: generateButton, withOffset: edgeMargin, relation:NSLayoutRelation.greaterThanOrEqual)
+        activityIndicator.autoPinEdge(.right, to: .left, of: validateButton, withOffset: -edgeMargin, relation:NSLayoutRelation.greaterThanOrEqual)
     }
     
     required init?(coder aDecoder: NSCoder)
